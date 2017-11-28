f47e792df669b47c4574e213dfee16b38f382e73
Tycho Lürsen <tycholursen@gmail.com>
2017-11-28 20:49:00 +0100

    Fix FE_GET_PROPERTY/FE_SET_PROPERTY usercopy logic
    The logic is broken, as it tries to access userspace data
    before copy. Tested with KASAN.
    
    Credits: Mauro Carvalho Chehab

--- dvbloopback/dvb_loopback.c
+++ dvbloopback/dvb_loopback.c
@@ -113,6 +113,54 @@ static void rvfree(void *mem, unsigned long size)
 	vfree(mem);
 }
 
+static int call_func(struct file *file,
+		     unsigned int cmd, void **parg,
+		     int (*func)(struct file *file, unsigned int cmd, void *arg))
+{
+	int err, msize;
+	struct dtv_properties *tvps = *parg;
+	struct dtv_property *tvp = NULL, *oldprops = NULL;
+
+	if ((cmd != FE_SET_PROPERTY) && (cmd != FE_GET_PROPERTY))
+		return func(file, cmd, *parg);
+
+	/* Put an arbitrary limit on the number of messages that can
+	 * be sent at once */
+	if ((tvps->num == 0) || (tvps->num > DTV_IOCTL_MAX_MSGS))
+		return -EINVAL;
+
+	msize = _IOC_SIZE(cmd) + tvps->num * sizeof(struct dtv_property);
+
+	/*
+	 * Since most part of the code assume that "parg" is a contious piece of
+	 * memory when FE_SET_PROPERTY / FE_GET_PROPERTY is used
+	 * It doesn't work with tvps->props attached as a pointer
+	 * it will be over written and recalculated as an offset to parg (tvps)
+	 * This is not a nice fix but it works  /H.Schillstrom
+	 */
+	tvps = krealloc(*parg, msize, GFP_KERNEL);
+	if (!tvps)
+		return -ENOMEM;
+
+	*parg = tvps;
+	tvp = *parg + _IOC_SIZE(cmd);
+
+	if (copy_from_user(tvp, tvps->props, tvps->num * sizeof(*tvp)))
+		return -EFAULT;
+
+	oldprops = tvps->props;
+	tvps->props = tvp;
+
+	err = func(file, cmd, *parg);
+
+	if (cmd == FE_GET_PROPERTY) {
+		if (copy_to_user((void __user *)oldprops, tvp,
+				 tvps->num * sizeof(struct dtv_property)))
+			err = -EFAULT;
+	}
+	return err;
+}
+
 /* This is a copy of dvb_usercopy.  We need to do this because it isn't exported
    by dvbdev
 */
@@ -123,13 +171,9 @@ static int dvblb_usercopy(struct file *file,
  		     unsigned int cmd, void *arg))
 
 {
-	char    sbuf[128];
 	void    *mbuf = NULL;
 	void    *parg = NULL;
 	int     err  = -EINVAL;
-	int	msize = 0;
-	struct  dtv_properties *tvps = NULL;
-	struct  dtv_property *tvp = NULL;
 
 	/*  Copy arguments into temp kernel buffer  */
 	switch (_IOC_DIR(cmd)) {
@@ -143,50 +187,19 @@ static int dvblb_usercopy(struct file *file,
 	case _IOC_READ: /* some v4l ioctls are marked wrong ... */
 	case _IOC_WRITE:
 	case (_IOC_WRITE | _IOC_READ):
-
-		/*
-		 * Since most part of the code assume that "parg" is a contious pice of
-		 * memory when FE_SET_PROPERTY / FE_GET_PROPERTY is used
-		 * It doesn't work with tvps->props attached as a pointer
-		 * it will be over written and recalculated as an offset to parg (tvps)
-		 * This is not a nice fix but it works  /H.Schillstrom
-		 */
-		msize = _IOC_SIZE(cmd);
-
-		if ((cmd == FE_SET_PROPERTY) || (cmd == FE_GET_PROPERTY)) {
-			tvps = (struct dtv_properties __user *)arg;
-			msize += tvps->num * sizeof(struct dtv_property);
-		}
-		if (msize < sizeof(sbuf))
-		    parg = sbuf;
-		else {
-		    mbuf = kmalloc(msize,  GFP_KERNEL);
-		    if (NULL == mbuf)
+		mbuf = kmalloc(_IOC_SIZE(cmd), GFP_KERNEL);
+		if (!mbuf)
 			return -ENOMEM;
-		    parg = mbuf;
-		}
+		parg = mbuf;
 
 		err = -EFAULT;
 		if (copy_from_user(parg, (void __user *)arg, _IOC_SIZE(cmd)))
 			goto out;
-		if ((cmd == FE_SET_PROPERTY) || (cmd == FE_GET_PROPERTY)) {
-		    tvps = (struct dtv_properties __user *)arg;
-		    tvp = (struct dtv_property *) (parg + _IOC_SIZE(cmd));
-
-		    if (copy_from_user(tvp, tvps->props,
-			(tvps->num) * sizeof(struct dtv_property))) {
-			err = -EFAULT;
-			goto out;
-		    }
-		    tvps = (struct dtv_properties __user *)parg;
-		    tvps->props = tvp;
-		    tvp = NULL;
-		}
 		break;
 	}
 
 	/* call driver */
-	if ((err = func(file, cmd, parg)) == -ENOIOCTLCMD)
+	if ((err = call_func(file, cmd, &parg, func)) == -ENOIOCTLCMD)
 		err = -ENOTTY;
 
 	if (err < 0)
@@ -197,25 +210,15 @@ static int dvblb_usercopy(struct file *file,
 	{
 	case _IOC_READ:
 	case (_IOC_WRITE | _IOC_READ):
-		if ((cmd == FE_GET_PROPERTY) || (cmd == FE_SET_PROPERTY))
-		{
-		    tvps = (struct dtv_properties __user *)arg;
-		    tvp = tvps->props;
-		    tvps = (struct dtv_properties __user *)parg;
-		    if (copy_to_user(tvp, tvps->props, tvps->num *
-			    sizeof(struct dtv_property))) {
-			err = -EFAULT;
-			goto out;
-		    }
-		    tvps->props = tvp;
-		}
 		if (copy_to_user((void __user *)arg, parg, _IOC_SIZE(cmd)))
 			err = -EFAULT;
 		break;
 	}
 
 out:
-	kfree(mbuf);
+	/* The krealloc() may have changed the buffer pointer */
+	if (mbuf)
+		kfree(parg);
 	return err;
 }
 
